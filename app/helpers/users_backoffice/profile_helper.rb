d15a89cd70b5ce21ba5b4c8413a2fa58283f0e1c
Amaury Martin <amaury.conde@hotmail.com>
2020-06-16 01:03:22 -0300

    Aula 215

--- app/helpers/users_backoffice/profile_helper.rb
+++ app/helpers/users_backoffice/profile_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module UsersBackoffice::ProfileHelper
+  def gender_select(user, current_gender)
+    user.user_profile.gender == current_gender ? 'btn-primary' : 'btn-default'
+  end
 end
